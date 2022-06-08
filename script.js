845471900b7ec71b66332a9db24f2e52ea4681f2
alphanse <alphanse98@gmail.com>
2022-06-08 10:30:07 +0530

    add-file

---
+++ script.js
@@ -0,0 +1,10 @@
+let p = parseInt(prompt(" Enter initial principal balance"))
+let r = parseInt(prompt(" Enter Annual interest rate")) 
+let n = parseInt(prompt(" Enter Time (in years)"))
+let ans = (p*n*r)/100
+console.log("ans =  ",ans )
+
+// A	=	final amount
+// P	=	initial principal balance
+// r	=	annual interest rate
+// n	=	time (in years) 
